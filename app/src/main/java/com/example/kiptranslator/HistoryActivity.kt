bfdfe1a45e555be9a99d71e65da2f66a390aaec7
ppodl <ppodlegaev@gmail.com>
2023-02-16 12:44:00 +0300

    build 1.21

--- app/src/main/java/com/example/kiptranslator/HistoryActivity.kt
+++ app/src/main/java/com/example/kiptranslator/HistoryActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
 
 class HistoryActivity : AppCompatActivity() {
+    lateinit var recycler: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+       recycler = findViewById(R.id.translateList)
 
         val btnBack: Button = findViewById(R.id.btnBackH)
         btnBack.setOnClickListener {
